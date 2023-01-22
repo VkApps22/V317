64a9e5c59ce60ad09c20adef49ef313e81ada1b8
Isabela Birges <isabela.birges@weareglobals.com>
2023-01-22 09:30:12 -0300

    Parsing Firebase Realtime Database

---
+++ app/src/main/java/br/com/kascosys/vulkanconnectv317/models/AlertsFirebaseModel.kt
@@ -0,0 +1,8 @@
+package br.com.kascosys.vulkanconnectv317.models
+
+data class AlertsFirebaseModel(
+    val language: String? = "",
+    val id: String? = "",
+    val description: String = "",
+    val label: String = ""
+)
